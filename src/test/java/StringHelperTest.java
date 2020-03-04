afb3bcfac8557375bc30ad3f00407c072a6dce16
Gabriel Valdes <gabriel_avd@hotmail.com>
2020-03-03 22:48:21 -0800

    Refactored

--- src/test/java/StringHelperTest.java
+++ src/test/java/StringHelperTest.java
@@ -7,10 +7,10 @@ public class StringHelperTest {
     @Test
     public void test(){
         StringHelper helper = new StringHelper();
-        String actual = helper.truncateAInFirst2Positions("AACB");
         String expected = "CB";
 
-        assertEquals(expected, actual);
+        assertEquals(expected, helper.truncateAInFirst2Positions("AACB"));
+        assertEquals(expected, helper.truncateAInFirst2Positions("ACB"));
 
     }
 }
